3e1a13fd7e1d1a1e9f272c5a3f8d12a7636f33d0
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-31 10:07:37 +0000

    Aug 31, 2026, 10:07 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,7 +6,7 @@ function setup(){
 function draw() {
 background(220)
 
-background(220)
+world.gravity.y = 50;
 if (mouse.presses()) {
     ball = new Sprite(mouse.x,mouse.y,50)
     ball.mass = 10;
