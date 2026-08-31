0c49018b008e4654b4d8c1422858f8ec4af8c497
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-31 10:10:10 +0000

    Aug 31, 2026, 10:10 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,15 @@
+let ball
+function setup(){
+ createCanvas(600, 400);
+ background(220);
+}
+function draw() {
+background(220)
+
+world.gravity.y = 50;
+if (mouse.presses()) {
+    ball = new Sprite(mouse.x,mouse.y,50)
+    ball.mass = 10;
+}
+}
 
